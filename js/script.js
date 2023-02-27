ff5b8ccae63eb419eca53056b2decb6bba2d5fb5
danilopiccinni <danilopiccinni@gmail.com>
2023-02-27 14:17:54 +0100

    feat: js lancio dei dati

--- js/script.js
+++ js/script.js
@@ -29,3 +29,39 @@ buttonEl.addEventListener('click' , function() {
         consentito =false
     }
 })
+
+
+// ----------------------------------------
+
+let numeroUtente;
+let numeroBot;
+let numeroUtenteEl = document.getElementById ('numero-utente')
+let numeroBotEl = document.getElementById ('numero-bot')
+let vincitoreEl = document.getElementById ('vincitore') 
+vincitoreEl.style.width='250px'
+
+let buttonLanciEl = document.getElementById('bottone-lancio')
+
+buttonLanciEl.addEventListener('click' , function() {
+
+    numeroUtente= Math.floor(Math.random() * 6 + 1)
+
+    numeroUtenteEl.value = numeroUtente
+
+    numeroBot = Math.floor(Math.random() * 6 + 1 )
+
+    numeroBotEl.value =numeroBot
+
+    console.log (numeroUtente)
+    console.log (numeroBot)
+
+    if (numeroUtente > numeroBot) {
+        vincitoreEl.value = "vittoria dell'utente con il " + numeroUtente
+    } else if (numeroUtente < numeroBot) {
+        vincitoreEl.value = "vittoria del computer con il " + numeroBot
+    }
+    else { 
+        vincitoreEl.value = 'pareggio'
+    }
+
+})
